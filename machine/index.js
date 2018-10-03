91953522d5dab5059d27d05c55cd59d2730f9caf
Alexis Colin <nesquimo@gmail.com>
2018-10-03 16:36:09 +0200

    SVG -> Canvas

--- machine/index.js
+++ machine/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
   var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
-    renderer: 'svg',
+    renderer: 'canvas',
     loop: true,
     autoplay: true,
     path: 'data.json'
