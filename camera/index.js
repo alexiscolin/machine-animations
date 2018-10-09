7192ca75e9e755898aab459ef9fc0d386e7e36fd
Alexis Colin <nesquimo@gmail.com>
2018-10-09 17:11:59 +0200

    Ajout script camera

---
+++ camera/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  });
+});
